949a37a674ecac72f063699809c92446593aa727
maktoumali <maktoumali999@gmail.com>
2026-08-27 12:22:12 +0530

    fix : supense added in the logout button commponent , for smoother user experince

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
@@ -50,7 +51,9 @@ export default function RootLayout({
           />
         </div>
         <Providers>
-          <LogoutButton />
+          <Suspense fallback={null}>
+            <LogoutButton />
+          </Suspense>
           {children}
         </Providers>
         <Toaster />
